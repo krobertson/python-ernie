946efaf667f3b3c2e7a9e6df30b65619581cc548
Andre Graf <andre@dergraf.org>
2011-10-28 14:46:14 +0200

    added comments to setup.py

--- setup.py
+++ setup.py
@@ -2,15 +2,27 @@
 
 from distutils.core import setup
 
+# The original author of this library is
+# Ken Robertson ken@invalidlogic.com
+# This version is a fork on github maintained by
+# Andre Graf andre@dergraf.org.
+#
+# Ken's library version number depended on
+# the version number of python-bert. This is
+# not suitable for an automated installation
+# using pip which will automatically install 
+# the dependencies listed in 'install_requires'.
+# For this reason I manually set the version
+# number.
 __version__ = '1.0.0'
 
 setup(
     name = 'ernie',
     version = __version__,
     description = 'BERT-Ernie Library',
-    author = 'Ken Robertson',
-    author_email = 'ken@invalidlogic.com',
-    url = 'http://invalidlogic.com',
+    author = 'Andre Graf',
+    author_email = 'andre@dergraf.org',
+    url = 'http://github.com/dergraf/python-ernie',
     packages = ['ernie'],
     classifiers = [
         'Intended Audience :: Developers',
